12282b6226884c97fe9b57687d51df08da909b08
Luigi Laezza <luigi@soiposervices.com>
2024-05-02 10:38:24 +0200

    Disabling pagination if not needed

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -11,7 +11,7 @@ defineProps({
 </script>
 
 <template>
-    <div class="join">
+    <div class="join" v-if="items.length > 3">
         <div v-for="item in items" >
             <Link v-if="item.url"  class="join-item btn text-white bg-gray-800 dark:bg-gray-200" :href="item.url" :class="{'btn-active' : item.active }" :key="item.id" v-html="item.label"/>
             <PrimaryButton v-else class="join-item btn bg-gray-800 dark:bg-gray-200" :key="item.id" v-html="item.label" disabled/>
