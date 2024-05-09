4b606402b3e177a8d8c1c89a2e6322034e50fad1
Luigi Laezza <luigi@soiposervices.com>
2024-05-09 11:18:48 +0200

    Adding also hashed password

--- app/Http/Controllers/LinkedinController.php
+++ app/Http/Controllers/LinkedinController.php
@@ -4,8 +4,10 @@ use Illuminate\Http\Request;
 use Exception;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 use Laravel\Socialite\Facades\Socialite;
-
+use Illuminate\Support\Str;
 use function Laravel\Prompts\error;
 
 class LinkedinController extends Controller
@@ -17,33 +19,41 @@ class LinkedinController extends Controller
 
     public function linkedinCallback()
     {
+
         try {
 
             $linkedinUser = Socialite::driver('linkedin-openid')->user();
 
             $user = User::where('oauth_id', $linkedinUser->id)->first();
             if($user->profile_photo_path){
-                $user->profile_photo_path = $linkedinUser->avatar;
+
+                $url = $linkedinUser->avatar;
+                $name = "profile-photos/".basename($url);
+                Storage::disk('public')->put($name, file_get_contents($url));
+
+                $user->profile_photo_path = $name;
             }
             if($user){
 
                 Auth::login($user);
 
-                return redirect('/dashboard');
-
+                return redirect()->route('dashboard');
             }else{
+
+                $password = Str::password();
+
                 $user = User::create([
                     'name' => $linkedinUser->name,
                     'email' => $linkedinUser->email,
                     'oauth_id' => $linkedinUser->id,
                     'profile_photo_path' => $linkedinUser->avatar,
                     'oauth_type' => 'linkedin',
-                    'password' => encrypt('admin12345')
+                    'password' =>  Hash::make($password)
                 ]);
 
                 Auth::login($user);
 
-                return redirect('/dashboard');
+                return redirect()->route('dashboard');
             }
 
         } catch (Exception $e) {
