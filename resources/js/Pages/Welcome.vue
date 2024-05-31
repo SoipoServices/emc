881406d903f1b1c4d7ba645bd1c75a77c7af05db
Luigi Laezza <luigi@soiposervices.com>
2024-05-31 14:12:25 +0200

    Working on new layout

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,5 @@
 <script setup>
-import AppLayout from '@/Layouts/PublicLayout.vue';
+import AppLayout from '@/Layouts/GuestLayout.vue';
 
 </script>
 
@@ -9,7 +9,7 @@ import AppLayout from '@/Layouts/PublicLayout.vue';
             <div
                 class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
             >
-                <div id="screenshot-container" class="relative flex w-full flex-1 items-stretch">
+                <div id="screenshot-container" class="relative flex items-stretch flex-1 w-full">
                     <img
                         src="../../images/event_pic.jpg"
                         alt="Laravel documentation screenshot"
@@ -41,7 +41,7 @@ import AppLayout from '@/Layouts/PublicLayout.vue';
                 class="flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
             >
                 <div class="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#FF2D20]/10 sm:size-16">
-                    <v-icon name="fa-facebook" class="w-12  h-12 sm:h-10 sm:w-10" animation="wrench" fill="#FF2D20" hover/>
+                    <v-icon name="fa-facebook" class="w-12 h-12 sm:h-10 sm:w-10" animation="wrench" fill="#FF2D20" hover/>
                 </div>
 
                 <div class="pt-3 sm:pt-5">
@@ -61,7 +61,7 @@ import AppLayout from '@/Layouts/PublicLayout.vue';
                 class="flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
             >
                 <div class="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#FF2D20]/10 sm:size-16">
-                    <v-icon name="fa-linkedin" class="w-12  h-12 sm:h-10 sm:w-10" animation="wrench" fill="#FF2D20" hover/>
+                    <v-icon name="fa-linkedin" class="w-12 h-12 sm:h-10 sm:w-10" animation="wrench" fill="#FF2D20" hover/>
                 </div>
 
                 <div class="pt-3 sm:pt-5">
