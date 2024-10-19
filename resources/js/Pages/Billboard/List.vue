dfb2d3e182201773fa41521e6e2eac788dadc60c
Luigi Laezza <luigi@soiposervices.com>
2024-10-19 11:34:31 +0200

    adding style fix

--- resources/js/Pages/Billboard/List.vue
+++ resources/js/Pages/Billboard/List.vue
@@ -81,8 +81,8 @@ const getReactionCounts = (post) => {
                                 </Link>
                                 <Link
                                     :href="route('billboard.show', post.id)"
-                                    class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition border border-transparent rounded-md dark:bg-white dark:text-gray-800 hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25"
-                                >
+                                    class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition bg-gray-800 border border-transparent rounded-md dark:bg-white dark:text-gray-800 hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25"
+                                    >
                                     Read more
                                 </Link>
                             </div>
