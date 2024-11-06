c149f711231b88e09f3dcd0adffae0554961e85f
Luigi Laezza <luigi@soiposervices.com>
2024-11-06 12:07:21 +0100

    removing Handler

--- app/Policies/BusinessPolicy.php
+++ app/Policies/BusinessPolicy.php
@@ -4,11 +4,9 @@ namespace App\Policies;
 
 use App\Models\Business;
 use App\Models\User;
-use Illuminate\Auth\Access\HandlesAuthorization;
 
 class BusinessPolicy
 {
-    use HandlesAuthorization;
 
     public function viewAny(User $user)
     {
--- app/Policies/EventPolicy.php
+++ app/Policies/EventPolicy.php
@@ -4,11 +4,9 @@ namespace App\Policies;
 
 use App\Models\Event;
 use App\Models\User;
-use Illuminate\Auth\Access\HandlesAuthorization;
 
 class EventPolicy
 {
-    use HandlesAuthorization;
 
     public function viewAny(User $user)
     {
