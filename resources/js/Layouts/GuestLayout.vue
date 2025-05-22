bc9ba1176dcae7c162fdc02311ffa5d185703707
Luigi Laezza <luigi@roarinc.com>
2025-05-22 10:05:29 +0200

    Adding feedback form

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -40,23 +40,27 @@ const logout = () => {
             <span class="sr-only">Entrepreneur Meet Cagliari</span>
             <nav class="flex gap-5 ml-auto sm:gap-6">
                 <NavLink v-if="$page.props.auth.user" :href="route('dashboard')">
-                        Dashboard
+                    Dashboard
+                </NavLink>
+                <div v-else>
+                    <NavLink :href="route('login')" class="mr-4">
+                        Log in
                     </NavLink>
-                    <div v-else>
-                        <NavLink :href="route('login')" class="mr-4">
-                            Log in
-                        </NavLink>
 
-                        <NavLink v-if="canRegister" :href="route('register')">
-                            Register
-                        </NavLink>
-                    </div>
+                    <NavLink v-if="canRegister" :href="route('register')">
+                        Register
+                    </NavLink>
+                </div>
                 <NavLink :href="route('events.index')">
-                        Events
-                        </NavLink>
-                        <NavLink :href="route('public.businesses.index')">
-                        Brands
-                        </NavLink>
+                    Events
+                </NavLink>
+                <NavLink :href="route('public.businesses.index')">
+                    Brands
+                </NavLink>
+                <a href="https://emcagliari.com/short/pVJp4" target="_blank"
+                    class="inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-gray-500 transition duration-150 ease-in-out border-b-2 border-transparent dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700">
+                    Feedback
+                </a>
             </nav>
         </header>
         <main class="flex-1">
