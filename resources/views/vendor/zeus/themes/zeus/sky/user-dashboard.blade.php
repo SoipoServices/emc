c93002f6704282593431022f6b313bc1bffb3e90
Luigi Laezza <luigi@soiposervices.com>
2025-08-21 11:39:22 +0200

    fix: Update user dashboard to display email as a clickable link and replace social media icons with heroicons

--- resources/views/vendor/zeus/themes/zeus/sky/user-dashboard.blade.php
+++ resources/views/vendor/zeus/themes/zeus/sky/user-dashboard.blade.php
@@ -43,7 +43,6 @@
                     @if($user->position)
                         <p class="mt-1 text-sm text-gray-600 dark:text-gray-300">{{ $user->position }}</p>
                     @endif
-                    <p class="mt-1 text-sm text-gray-600 dark:text-gray-300">{{ $user->email }}</p>
                     
                     {{-- Location Information --}}
                     @if($user->city || $user->country)
@@ -64,51 +63,44 @@
                     
                     <!-- Social Media Links and Contact -->
                     <div class="flex items-center gap-4 mt-3">
+                        <!-- Email -->
+                        <a href="mailto:{{ $user->email }}" class="text-gray-500 transition-colors hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300" title="Email: {{ $user->email }}">
+                            <x-heroicon-o-envelope class="w-5 h-5" />
+                        </a>
+                        
                         @if($user->site_url)
                             <a href="{{ $user->site_url }}" target="_blank" class="text-gray-500 transition-colors hover:text-blue-600 dark:text-gray-400 dark:hover:text-blue-400" title="Website">
-                                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M4.083 9h1.946c.089-1.546.383-2.97.837-4.118A6.004 6.004 0 004.083 9zM10 2a8 8 0 100 16 8 8 0 000-16zm0 2c-.076 0-.232.032-.465.262-.238.234-.497.623-.737 1.182-.389.907-.673 2.142-.766 3.556h3.936c-.093-1.414-.377-2.649-.766-3.556-.24-.559-.499-.948-.737-1.182C10.232 4.032 10.076 4 10 4zm3.971 5c-.089-1.546-.383-2.97-.837-4.118A6.004 6.004 0 0115.917 9h-1.946zm-2.003 2H8.032c.093 1.414.377 2.649.766 3.556.24.559.499.948.737 1.182.233.23.389.262.465.262.076 0 .232-.032.465-.262.238-.234.497-.623.737-1.182.389-.907.673-2.142.766-3.556zm1.166 4.118c.454-1.148.748-2.572.837-4.118h1.946a6.004 6.004 0 01-2.783 4.118zm-6.268 0C6.412 13.97 6.118 12.546 6.03 11H4.083a6.004 6.004 0 002.783 4.118z" clip-rule="evenodd"/>
-                                </svg>
+                                <x-heroicon-o-globe-alt class="w-5 h-5" />
                             </a>
                         @endif
                         
                         @if($user->twitter_url)
                             <a href="{{ $user->twitter_url }}" target="_blank" class="text-gray-500 transition-colors hover:text-blue-400 dark:text-gray-400 dark:hover:text-blue-300" title="Twitter">
-                                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-                                    <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"/>
-                                </svg>
+                                <x-heroicon-s-hashtag class="w-5 h-5" />
                             </a>
                         @endif
                         
                         @if($user->facebook_url)
                             <a href="{{ $user->facebook_url }}" target="_blank" class="text-gray-500 transition-colors hover:text-blue-600 dark:text-gray-400 dark:hover:text-blue-400" title="Facebook">
-                                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-                                    <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
-                                </svg>
+                                <x-heroicon-o-user-group class="w-5 h-5" />
                             </a>
                         @endif
                         
                         @if($user->linkedin_profile_url)
                             <a href="{{ $user->linkedin_profile_url }}" target="_blank" class="text-gray-500 transition-colors hover:text-blue-700 dark:text-gray-400 dark:hover:text-blue-500" title="LinkedIn">
-                                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-                                    <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
-                                </svg>
+                                <x-heroicon-o-briefcase class="w-5 h-5" />
                             </a>
                         @endif
                         
                         @if($user->telephone)
                             <a href="tel:{{ $user->telephone }}" class="text-gray-500 transition-colors hover:text-green-600 dark:text-gray-400 dark:hover:text-green-400" title="Phone: {{ $user->telephone }}">
-                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"/>
-                                </svg>
+                                <x-heroicon-o-phone class="w-5 h-5" />
                             </a>
                         @endif
                         
                         <!-- vCard Download -->
                         <a href="{{ route('user.vcard', $user->id) }}" class="text-gray-500 transition-colors hover:text-purple-600 dark:text-gray-400 dark:hover:text-purple-400" title="Download vCard">
-                            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/>
-                            </svg>
+                            <x-heroicon-o-arrow-down-tray class="w-5 h-5" />
                         </a>
                     </div>
                 </div>
